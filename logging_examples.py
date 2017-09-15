af3bb14f63a1dc2cbd4b2eef9e37caa44e468c10
alexgids <alex.gids@gmail.com>
2017-09-15 16:22:27 +0530

    Update logging_examples.py

--- logging_examples.py
+++ logging_examples.py
@@ -1,13 +1,5 @@
-import ConfigParser
+import configparser
+config = configparser.ConfigParser()
+config.read('conf.ini')
 
-configParser = ConfigParser.RawConfigParser()   
-configFilePath = r'c:\abc.txt'
-configParser.read(configFilePath)
- 
-self.path = configParser.get('your-config', 'path1')\
-
-
-[your-config]
-path1 = "D:\test1\first"
-path2 = "D:\test2\second"
-path3 = "D:\test2\third"
+config.get('file-path','path1')
